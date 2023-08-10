c8508966c89d71777cd27c016d4d3a0410895df7
Matthew Berens <matthewjberens@gmail.com>
2023-08-10 10:59:51 -0400

    udpate

--- code/CoBRA_porewater_plotting.R
+++ code/CoBRA_porewater_plotting.R
@@ -12,7 +12,9 @@ source("code/0-packages.R")
 
 # Step 1. Load Data ---------------------------------------------------------------------
 
-sampling_data = read.csv("porewater data/processed/CoBRA POREWATER_Mar2023.csv") 
+sampling_data_Mar = read.csv("porewater data/processed/CoBRA POREWATER_Mar2023.csv") 
+sampling_data_July = read.csv("porewater data/processed/CoBRA POREWATER_July2023.csv") 
+
 
 
 ######################################
@@ -27,12 +29,13 @@ wl_all <- rbind(relative_wl_July2023, relative_wl_Mar2023) %>%
 wl_all %>%
   ggplot(aes(x = distance, y = -depth)) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "Blue") +
-  geom_point()+
+  #geom_point()+
+  stat_summary(shape = 21, fill = "brown", size = 1) +
   theme_mb1() +
   facet_wrap(transect~month, 
              nrow=2, 
              scales = "free_x") +
-  labs(y = "Relative water level (cm)", x = "Plot") +
+  labs(y = "Relative water level (cm)", x = "Distance (m)") +
   theme(aspect.ratio = 0.5)
 
 
@@ -40,9 +43,9 @@ wl_all %>%
 
 
 ### OLD TRANSECT PLOTS ######
-sampling_data %>%
+rbind(sampling_data_July, sampling_data_Mar) %>%
   subset(transect != "blank") %>%
-  subset(parameter %in% c("SpC", "pH", "ORP", "Fe_II", "DIC")) %>%
+  subset(parameter %in% c("pH")) %>%
   ggplot(aes(y=depth, x=result_value)) +
   geom_hline(yintercept = 0, linetype = "dashed", color = "Blue") +
   geom_point(aes(fill=as.factor(plot)), shape=21, size = 3) +
@@ -57,7 +60,23 @@ sampling_data %>%
   expand_limits(x = 0)
 
 
-
+### OLD TRANSECT PLOTS ######
+rbind(sampling_data_July, sampling_data_Mar) %>%
+  subset(transect != "blank") %>%
+  subset(parameter == "pH") %>%
+  ggplot() +
+  geom_boxplot(aes(x=as.factor(transect), y = result_value, fill = transect)) +
+  #geom_hline(yintercept = 0, linetype = "dashed", color = "Blue") +
+  #geom_point(aes(y = depth, x = result_value, fill=as.factor(plot)), shape=21, size = 3) +
+  labs(y = "Depth (cm)", x = NULL, fill = "Plot") +
+  #facet_grid(~transect, 
+            #scales = "free") +
+  theme_mb1() +
+  #scale_y_reverse() +
+  #cale_x_continuous(position = "top") +
+  #scale_fill_manual(values = colors_young) +
+  #scale_color_manual(values = colors_young) +
+  expand_limits(x = 0)
 
 
 
